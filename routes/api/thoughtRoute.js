b0fe0870db6630330a85ed9e0db620394b0a335a
DUN <bobgonzalez516@gmail.com>
2023-10-24 16:40:10 -0600

    little stuff

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -45,4 +45,8 @@ router.put('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+router.delete('/:id',async (req, res) => {
+  const deletedThought = Thought.deleteOne({})
 })
